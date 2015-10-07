452fd31a5f1c467880d7bc5392f2a7f7382006af
rjos <RodolfoSoares@rodolfo-jose.local>
2015-10-07 11:42:21 -0300

    Update BaseSkill

--- MiraadTales/MiraadTales/BaseSkill.swift
+++ MiraadTales/MiraadTales/BaseSkill.swift
@@ -11,12 +11,16 @@ import UIKit
 public class BaseSkill: NSObject {
 
     public let name: String
+    public let expendMP: Int
     public let ownerRace: PlayersRace
+    public let requiredLvl: Int
     public let effects: String
     
-    public init(name:String, ownerRace:PlayersRace, effects: String) {
+    public init(name:String, expendMP: Int,ownerRace:PlayersRace, requiredLvl:Int,effects: String) {
         self.name = name
+        self.expendMP = expendMP
         self.ownerRace = ownerRace
+        self.requiredLvl = requiredLvl
         self.effects = effects
     }
 }
