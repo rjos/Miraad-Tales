269e71541971f5df8dce8be47b1669fb8af05ea8
rjos <RodolfoSoares@rodolfo-jose.local>
2015-10-09 11:58:11 -0300

    Add joystick and sprite de Ylla.

--- MiraadTales/MiraadTales/GameScene.swift
+++ MiraadTales/MiraadTales/GameScene.swift
@@ -9,37 +9,97 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var isWalking = false
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!";
+//        myLabel.fontSize = 45;
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+//        
+//        self.addChild(myLabel)
         
-        self.addChild(myLabel)
+        let status = PlayerStatus(HP: 12, MP: 6, Speed: 16, pAtk: 16, mAtk: 6, pDef: 12, mDef: 6)
+        let ylla = Swordsman(name: "Ylla", status: status, equipments: [Equip](), skills: [BaseSkill]())
+        
+        let player = Player(race: ylla, imageNamed: "Ylla-2")
+        player.name = "player"
+        self.addChild(player)
+        
+        let joyBack = SKSpriteNode(imageNamed: "dpad")
+        let joyFront = SKSpriteNode(imageNamed: "joystick")
+        
+        joyBack.alpha = 0.5
+        joyFront.alpha = 0.7
+        
+        let joystick = Joystick(thumb: joyFront, andBackdrop: joyBack)
+        joystick.name = "joystick"
+        joystick.position = CGPointMake(-193.161, -132.619)
+        
+        let camera = self.childNodeWithName("SKCameraNode") as! SKCameraNode
+        camera.position = player.position
+        camera.addChild(joystick)
+        
+        //self.addChild(joystick)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+//        for touch in touches {
+//            let location = touch.locationInNode(self)
+//            
+//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
+//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        let camera = self.childNodeWithName("SKCameraNode") as! SKCameraNode
+        let joystick = camera.childNodeWithName("joystick") as! Joystick
+        
+        let player = self.childNodeWithName("player") as! Player
+        
+        var lastedPositionPlayer = player.position
+        lastedPositionPlayer = CGPointMake(lastedPositionPlayer.x + (joystick.velocity.x / 10), lastedPositionPlayer.y + (joystick.velocity.y / 10))
+        
+        if !self.isWalking && joystick.isTracking && joystick.velocity != CGPointZero {
+            
+            self.isWalking = !self.isWalking
+            
+            print(joystick.angularVelocity)
+            
+            if joystick.angularVelocity > 1 && joystick.angularVelocity < 2 {
+                player.walkingPlayer(DirectionPlayer.Right)
+            }else if joystick.angularVelocity < -1 && joystick.angularVelocity > -2 {
+                player.walkingPlayer(DirectionPlayer.Left)
+            }else if joystick.angularVelocity > -1 && joystick.angularVelocity < 1 {
+                player.walkingPlayer(DirectionPlayer.Up)
+            }else if joystick.angularVelocity < -2 || joystick.angularVelocity > 2 {
+                player.walkingPlayer(DirectionPlayer.Down)
+            }
+        }
+        
+        if !joystick.isTracking {
+            
+            self.isWalking = false
+            player.removeAction()
+        }
+        
+        player.position = lastedPositionPlayer
+        camera.position = player.position
     }
 }
--- MiraadTales/MiraadTales/Player.swift
+++ MiraadTales/MiraadTales/Player.swift
@@ -8,14 +8,28 @@
 
 import SpriteKit
 
-public class Player: SKSpriteNode {
+public enum DirectionPlayer: String {
+    case Up = "DirectionUp"
+    case Down = "DirectionDown"
+    case Left = "DirectionLeft"
+    case Right = "DirectionRight"
+}
+
+public class Player: SKNode {
     
     public let race: BaseRace
+    private var sprite: SKSpriteNode
+    private var playerWalkingFrames = Array<Array<SKTexture>>()
     
     public init(race: BaseRace, imageNamed: String) {
         self.race = race
-        let texture = SKTexture(imageNamed: imageNamed)
-        super.init(texture: texture, color: UIColor.redColor(), size: texture.size())
+        self.sprite = SKSpriteNode()
+        super.init()
+        self.setAtlas()
+        self.sprite = SKSpriteNode(texture: self.playerWalkingFrames[0][1])
+        self.addChild(self.sprite)
+        self.xScale = 0.5
+        self.yScale = 0.5
     }
 
     required public init?(coder aDecoder: NSCoder) {
@@ -24,5 +38,50 @@ public class Player: SKSpriteNode {
     
     public func update(currentTime: CFTimeInterval) {
         
+    }
+    
+    private func setAtlas() {
+        let playerAnimatedAtlas = SKTextureAtlas(named: self.race.name)
+        var walkFrames = [SKTexture]()
+        
+        let numImages = playerAnimatedAtlas.textureNames.count
+        
+        for var i = 0; i < numImages; i = i + 3 {
+            for var j = 1; j <= 3; j++ {
+                let playerTextureName = "\(self.race.name)\(i + j)"
+                walkFrames.append(playerAnimatedAtlas.textureNamed(playerTextureName))
+            }
+            
+            self.playerWalkingFrames.append(walkFrames)
+            walkFrames.removeAll()
+        }
+    }
+    
+    public func walkingPlayer(direction: DirectionPlayer) {
+        
+        self.removeAction()
+        
+        let walkingFramesDirection: [SKTexture]
+        
+        switch direction {
+        case .Up:
+            walkingFramesDirection = self.playerWalkingFrames[3]
+            break
+        case .Down:
+            walkingFramesDirection = self.playerWalkingFrames[0]
+            break
+        case .Left:
+            walkingFramesDirection = self.playerWalkingFrames[2]
+            break
+        case .Right:
+            walkingFramesDirection = self.playerWalkingFrames[1]
+            break
+        }
+        
+        self.sprite.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(walkingFramesDirection, timePerFrame: 0.1, resize: false, restore: true)), withKey: "walkingINPlacePlayer\(self.name)")
+    }
+    
+    public func removeAction() {
+        self.sprite.removeAllActions()
     }
 }
