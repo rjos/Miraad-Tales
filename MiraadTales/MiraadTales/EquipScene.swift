398a364e09ca0c8d15623267850280ce65b5092c
CInIce <ltb@cin.ufpe.br>
2015-10-22 12:06:08 -0300

    Please enter the commit message for your changes. Lines starting
    Starting all EquipScene

--- MiraadTales/MiraadTales/EquipScene.swift
+++ MiraadTales/MiraadTales/EquipScene.swift
@@ -6,5 +6,33 @@
 //  Copyright © 2015 Rodolfo José. All rights reserved.
 //
 
-import Foundation
+
+import SpriteKit
+
+public class EquipScene: SKScene{
+    
+    public override func didMoveToView(view: SKView) {
+        
+    }
+    
+    public override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    public override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    public override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    public override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        
+    }
+    
+    public override func update(currentTime: NSTimeInterval) {
+        
+    }
+}
 
