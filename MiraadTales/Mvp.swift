310f7349e6650ff7d017d7440bd189a5d1a88aed
rjos <RodolfoSoares@Rodolfo-Jose.local>
2015-11-05 12:42:24 -0300

    Create map MPV

---
+++ MiraadTales/Mvp.swift
@@ -0,0 +1,13 @@
+//
+//  Mvp.swift
+//  MiraadTales
+//
+//  Created by Rodolfo José on 05/11/15.
+//  Copyright © 2015 Rodolfo José. All rights reserved.
+//
+
+import UIKit
+
+class Mvp: SKScene {
+
+}
