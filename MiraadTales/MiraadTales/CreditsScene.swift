214eff05bdb9a3738998ab76cf207d9cac0317c6
rjos <RodolfoSoares@Rodolfo-Jose.local>
2015-11-30 12:34:09 -0300

    add new scene

---
+++ MiraadTales/MiraadTales/CreditsScene.swift
@@ -0,0 +1,13 @@
+//
+//  CreditsScene.swift
+//  MiraadTales
+//
+//  Created by Rodolfo José on 30/11/15.
+//  Copyright © 2015 Rodolfo José. All rights reserved.
+//
+
+import Cocoa
+
+class CreditsScene: SKScene {
+
+}
