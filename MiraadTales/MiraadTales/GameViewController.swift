bc89579ecbbc6df79c036d63f7b5be0e58afc290
rjos <RodolfoSoares@Rodolfo-Jose.local>
2015-12-06 15:58:26 -0300

    fixed issue

--- MiraadTales/MiraadTales/GameViewController.swift
+++ MiraadTales/MiraadTales/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let scene = MvpScene(fileNamed: "MvpScene") {
+        if let scene = Start(fileNamed: "Start") {
             // Configure the view.
             let skView = self.view as! NavigationController
             
